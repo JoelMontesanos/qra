6db480bc4469665c2193509018795fdcedd7b74e
Joel Montesano <joel.montesanos@gmail.com>
2022-01-21 14:50:13 -0600

    Feed: Alerts, need to fix, not working

--- src/controllers/qr.controller.js
+++ src/controllers/qr.controller.js
@@ -4,32 +4,42 @@ const Qr = require('../models/qr_model');
 qrCtl.renderQrForm = (req, res)=>{
     res.render('qrs/new_qr');
 };
+
 qrCtl.createNewQr = async (req,res)=> {
     const {nombre,ine,auto,placas,casaVisita,motivo,validez} = req.body;
     const newQr = new Qr({nombre,ine,auto,placas,casaVisita,motivo,validez});
     await newQr.save();
+    // Message
+    req.flash('success_message', 'Código Qr agregado satisfactoriamente');
     console.log(newQr);
     //console.log(req.body);// recepción de los datos, se encuentran en req.body
     res.redirect('/qr');
 };
+
 qrCtl.renderQr = async (req,res)=>{ // To get all qr 
     const qrs = await Qr.find().lean();
     res.render('qrs/all_qr',{qrs});
     //res.send('all qrs');
 };
+
 qrCtl.renderEditQr = async (req,res)=>{
     const qr = await Qr.findById(req.params.id).lean();
     console.log(qr);
     res.render('qrs/edit_qr', {qr});
 };
+
 qrCtl.updateQr = async (req,res) => {
     //destructuring
     const {nombre,ine,auto,placas,casaVisita,motivo,validez} = req.body
     await Qr.findByIdAndUpdate(req.params.id, {nombre,ine,auto,placas,casaVisita,motivo,validez});
+    req.flash('success_message','Qr actializado Satisfactoriamente');
     res.redirect('/qr');
 };
+
 qrCtl.deleteQr = async (req,res)=>{
     await Qr.findByIdAndDelete(req.params.id);
+    req.flash('success_message','Qr eliminado satisfactoriamente');
     res.redirect('/qr');
 }
+
 module.exports = qrCtl;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const { renderQrForm } = require('./controllers/qr.controller');
+
 //initialization
 const app = express();
 
@@ -21,12 +25,23 @@ app.set('view engine', '.hbs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'jesica',
+    resave:true,
+    saveUninitialized: true,
+}));
+app.use(flash());
+
 
 //Routes
 app.use(require('./routes/index.routes'));
 app.use(require('./routes/qr.routes'));
 
 //Global Variables
+app.use((req,res,next)=>{
+    res.locals.success_message = req.flash('success_message');
+    next();
+});
 
 
 //Static files
