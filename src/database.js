1aed15cef1a44171505539b6a41eea021da64bad
Joel Montesanos <joelmontesanos@MacBook-Air-de-Joel.local>
2023-08-28 19:48:27 -0600

    Fixed: database connection

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { QRA_MONGODB_DATABASE } = process.env;
 
 // Conexión a MongoDB en el sistema host (localhost)
-const MONGODB_URI = `mongodb://<IP_DE_TU_INSTANCIA>:27017/${QRA_MONGODB_DATABASE}`;
+const MONGODB_URI = `mongodb://3.135.134.158:27017/${QRA_MONGODB_DATABASE}`;
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
