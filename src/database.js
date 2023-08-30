9b85dece767ea9ed3ddec41b65c7869da35a39f5
Joel Montesanos <joelmontesanos@MacBook-Air-de-Joel.local>
2023-08-30 14:50:38 -0600

    fix: auth

--- src/database.js
+++ src/database.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose');
 
-const {QRA_MONGODB_HOST,
-    QRA_MONGODB_DATABASE} = process.env;
+const MONGODB_URI = 'mongodb://mongo:27017';
 
-//const MONGODB_URI = `mongodb://${QRA_MONGODB_HOST}/${QRA_MONGODB_DATABASE}`;
-const MONGODB_URI = `mongodb://mongo:27017/qr`;
-
-mongoose.connect(MONGODB_URI,{
-    useUnifiedTopology: true,
-    useNewUrlParser:true,
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 })
 .then(db => console.log('Database is connected'))
 .catch(err => console.log(err));
